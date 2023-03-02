3f7a1e1729981f6262abcefc3401c81ca46e386f
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2023-03-02 09:34:51 +0100

    update scripts.js
    creating an array with three pokemons and looping throw the array if the pokemon is bigger than six we will write this pokemon is big

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
-
+// Creating Array with three pokemon all pokemon detail from https://pokedex.org/
     let pokemonList  = [
         { name: 'Bulbasaur', height: 7, types : ['grass', 'poison'] },
         { name: 'Charmander', height: 6, types : ['Fire'] },
         { name: 'Squirtle', height: 5, types : ['Water4'] },
         
       ];
+for(let i=0; i<pokemonList.length; i++){
 
+
+  //looping throw the pokemon list 
+  document.write(pokemonList[i].name + " height: " + pokemonList[i].height + "<br>");
+  //check if the pokemon very big 
+  if(pokemonList[i].height > 6  ) {
+    document.write(pokemonList[i].name +  "<span> is big </span> <br>")
+  }
+}
